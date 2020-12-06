aed771d2531a36023b6b10f16e9bbe35e8171ae7
NelsonPosada <posada.nj@gmail.com>
2020-12-06 16:31:24 -0500

    Primer Commit

---
+++ src/main/java/com/proteccion/springboot/backend/apirest/image/SpringBootBackendApirestProcessImageApplication.java
@@ -0,0 +1,13 @@
+package com.proteccion.springboot.backend.apirest.image;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendApirestProcessImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackendApirestProcessImageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proteccion/springboot/backend/apirest/image/SpringBootBackendApirestProcessImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proteccion.springboot.backend.apirest.image;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendApirestProcessImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
